a6c5a04f858fdd9e4eb4d84435ab094ba21205df
root <root@ip-10-42-22-229.ec2.internal>
2025-11-03 17:02:02 +0000

    update task 2

--- simple_shell.c
+++ simple_shell.c
@@ -1,7 +1,35 @@
 #include "shell.h"
 /**
- * shell_proj - 
- *
- *
+ * shell_proj - UNIX command line interpreter
+ * Return: always 0
  */
+int shell_proj(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	pid_t pid;
 
+	while (1)
+	{
+	printf("#cisfun$ ");
+	if (getline(&line, &len, stdin) == -1)
+	break;
+
+	if (line[0] == '\n')
+	continue;
+	line[strcspn(line, "\n")] = '\0';
+	pid = fork();
+	if (pid == 0)
+	{
+	char *argv[] = {line, NULL};
+
+	execve(line, argv, NULL);
+	perror(line);
+	exit(1);
+	}
+	else
+	wait(NULL);
+	}
+	free(line);
+	return (0);
+}
