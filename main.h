4fed146c8e785153247287034bc4f9792f371c3a
“had271” <Hadeelmesnad@gmail.comgit config --global user.name had271git config --global user.email Hadeelmesnad@gmail.com>
2025-11-05 15:33:41 +0000

    edit main.h

--- main.h
+++ main.h
@@ -24,4 +24,5 @@ int execute(char **args);
 int empty_line(char *buff);
 extern char **environ;
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
