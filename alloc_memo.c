3b2aebb0ff57b6181bf33016bd5b451806602d24
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-05 18:20:08 +0300

    Update alloc_memo.c

--- alloc_memo.c
+++ alloc_memo.c
@@ -1,27 +1,55 @@
 #include "main.h"
 
+#include "shellheader.h"
+
 /**
- * _calloc - function that allocates memory for an array, using malloc
- * @nmemb: array
- * @size: size
- * Return: pointer or NULL
- */
+  * _realloc -  function to realloc space of memory
+  * @ptr: pointer holding what we want to realloc
+  * @old_size: size of the first string
+  * @new_size: size of the new string
+  * Return: new string
+  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int index = 0;
-	char *ptr = NULL;
+	void *tmp;
+	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	ptr = malloc(nmemb * size);
-
+	if (new_size == old_size)
+		return (ptr);
 	if (ptr == NULL)
+	{
+		tmp = malloc(new_size);
+		if (tmp == NULL)
+			return (NULL);
+		return (tmp);
+	}
+	if ((new_size == 0) && (ptr != NULL))
+	{
+		free(ptr);
 		return (NULL);
-
-	for (; index < (nmemb * size); index++)
-		ptr[index] = 0;
-
-	return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		tmp = malloc(new_size);
+		if (tmp == NULL)
+			return (NULL);
+		for (i = 0; i < old_size; i++)
+			((char *)tmp)[i] = ((char *)ptr)[i];
+		free(ptr);
+		return (tmp);
+	}
+	else
+	{
+		tmp = malloc(new_size);
+		if (tmp == NULL)
+			return (NULL);
+		for (i = 0; i < new_size; i++)
+		{
+			((char *)tmp)[i] = ((char *)ptr)[i];
+		}
+		free(ptr);
+		return (tmp);
+	}
+	return (NULL);
 }
