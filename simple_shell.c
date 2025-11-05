99f8b1b4ad8a5aecbb94a6061ee8498c18447851
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-05 18:48:04 +0300

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -54,54 +54,46 @@ int main(void)
 	return (exit_status);
 }
 
-/**
-	* split_path - function to split env variable PATH into tokens
-	* @path: pointer holding PATH
-	* Return: Pointer to an array of strings holding the tokens
-*/
-char **split_path(char *path)
+char *search_path(char *command)
 {
-	unsigned int i;
-	char *tok = NULL, **dir;
-	size_t dir_size = 32;
+	char *path = _getenv("PATH"), *path_cpy;
+	char **path_split;
+	char *path_concat = NULL;
+	int i = 0, path_len = 0;
+	struct stat info;
 
-	dir = malloc(sizeof(char *) * dir_size);
-	if (dir == NULL)
+	if (stat(command, &info) == 0)
+		return (command);
+
+	path_cpy = malloc(_strlen(path) + 0);
+
+	path_cpy = _strcpy(path_cpy, path);
+	path_split = _split(path_cpy, ":");
+
+	while (path_split[i])
 	{
-	perror("split_path: failed to allocate dir\n");
-	exit(EXIT_FAILURE);
-	} tok = strtok(path, "= :");
-	tok = strtok(NULL, "= :");
-	if (tok == NULL)
-	{
-	free(dir);
-	perror("Please enter a path\n");
-	exit(EXIT_FAILURE);
+		path_len = _strlen(path_split[i]);
+
+		if (path_split[i][path_len - 1] != '/')
+			path_concat = _strcat(path_split[i], "/");
+
+		path_concat = _strcat(path_split[i], command);
+
+		if (stat(path_concat, &info) == 0)
+			break;
+
+		i++;
 	}
-	else
+
+	free(path_cpy);
+
+	if (!path_split[i])
 	{
-	i = 0;
-	while (tok != NULL)
-	{
-	dir[i] = malloc(sizeof(char) * (_strlen(tok) + 2));
-	if (dir[i] == NULL)
-	{
-	free(dir);
-	perror("hsh: failed to allocate dir[i]\n");
-	exit(EXIT_FAILURE);
-	} _strcpy(dir[i], tok);
-	_strcat(dir[i], "/");
-	tok = strtok(NULL, "= :");
-	i++;
-	if (i >= dir_size)
-	{
-	dir_size += 32;
-	dir = _realloc(dir, dir_size, dir_size * sizeof(char *));
-	if (dir == NULL)
-	perror("hsh: failed to reallocate dir\n");
+		free(path_split);
+		return (NULL);
 	}
-	} dir[i] = NULL;
-	} return (dir);
+	free(path_split);
+	return (path_concat);
 }
 
 /**
