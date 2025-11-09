333cc0cab4616cfb88b8ea109520aa9e50144cc4
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-10 02:42:15 +0300

    Update help_func2.c

--- help_func2.c
+++ help_func2.c
@@ -40,7 +40,7 @@ ssize_t _getline(char **lineptr, size_t *n)
 		if (i >= (int)(*n - 1))
 		{
 			*n *= 2;
-			*lineptr = realloc(*lineptr, *n);
+			*lineptr =_realloc(*lineptr, old_size, *n);
 			if (*lineptr == NULL)
 				return (-1);
 		}
