637a3ee1df1c74d270c0e3c3c3245c823e0df9b8
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-10 02:40:56 +0300

    Update alloc_memo.c

--- alloc_memo.c
+++ alloc_memo.c
@@ -34,3 +34,32 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	* @new_size: size of the new string
 	* Return: new string
 	*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+    unsigned int i;
+    char *new_ptr, *old_ptr;
+
+    if (new_size == old_size)
+        return (ptr);
+
+    if (new_size == 0 && ptr)
+    {
+        free(ptr);
+        return (NULL);
+    }
+
+    if (!ptr)
+        return (malloc(new_size));
+
+    new_ptr = malloc(new_size);
+    if (!new_ptr)
+        return (NULL);
+
+    old_ptr = ptr;
+    for (i = 0; i < old_size && i < new_size; i++)
+        new_ptr[i] = old_ptr[i];
+
+    free(ptr);
+
+    return (new_ptr);
+}
