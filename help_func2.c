38361c5801fba644fbb77f1eee35e449e9cafd64
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-11-10 18:03:47 +0300

    Refactor myshell functions and add command reader
    Refactored myshell functions and added read_command function.

--- help_func2.c
+++ help_func2.c
@@ -12,7 +12,7 @@ int myshell_cd(char **args)
 	write(STDOUT_FILENO, "Please enter a command\n", 23);
 	else if (chdir(args[1]) != 0)
 	{
-		perror("hsh");
+		perror("cisfun");
 		exit(EXIT_FAILURE);
 	}
 return (1);
@@ -28,7 +28,7 @@ int myshell_env(__attribute__((unused)) char **args)
 {
 	char **envir;
 
-	for (envp = environ; *envir; envir++)
+	for (envir = environ; *envir; envir++)
 	{
 		write(STDOUT_FILENO, *envir, _strlen(*envir));
 		write(STDOUT_FILENO, "\n", 1);
@@ -46,15 +46,8 @@ int myshell_help(__attribute__((unused)) char **args)
 {
 	write(STDOUT_FILENO, "Help list \n", 13);
 	write(STDOUT_FILENO, "cd - change the current directory\n", 36);
-	write(STDOUT_FILENO, " example: cd .., cd [directorypath]\n", 35);
-	write(STDOUT_FILENO, "\n", 1);
 	write(STDOUT_FILENO, "  exit - cause normal process termination\n", 42);
-	write(STDOUT_FILENO, "  example: exit\n", 16);
-	write(STDOUT_FILENO, "\n", 1);
 	write(STDOUT_FILENO, "  help - display useful information\n", 36);
-	write(STDOUT_FILENO, "  example: help\n", 16);
-	write(STDOUT_FILENO, "\n", 1);
-	write(STDOUT_FILENO, "  Also support commands like\n", 29);
 	write(STDOUT_FILENO, "  cp - copy files and directories\n", 35);
 	write(STDOUT_FILENO, " clear - clear the terminal screen\n", 36);
 	write(STDOUT_FILENO, " cat - print on the standard output\n", 37);
@@ -65,12 +58,31 @@ return (1);
 
 /**
  * myshell_exit - function to exit from shell
- * @args: pointer to an array of pointers holding the arguments
+ * @args: pointer
  * Return: 0, always success
 */
 
 int myshell_exit(__attribute__((unused)) char **args)
 {
 	return (2);
-	/*return (0);*/
+}
+
+/**
+ * read_command - read the command that the user will type
+ * in my shell and stores it in a buffer
+ * Return: the buffer
+*/
+
+char *read_command(void)
+{
+	size_t line_size = 32;
+	char *line = NULL;
+
+	line = malloc(sizeof(char) * line_size);
+	if (getline(&line, &line_size, stdin) == EOF)
+	{
+		free(line);
+		exit(0);
+	}
+return (line);
 }
