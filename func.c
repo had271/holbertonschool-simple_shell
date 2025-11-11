589090bb4e683251ef0a386f7f3c13e2ec2373f3
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-11 10:26:58 +0300

    Update func.c

--- func.c
+++ func.c
@@ -69,3 +69,26 @@ int main(int argc, char **argv, char **envp)
 	free(line);
 	return (status);
 }
+
+
+/**
+	* parse_command - tokenizes a command line into arguments
+	* @line: input line
+	* @args: array to store tokens
+	*
+	* Return: number of arguments
+	*/
+int parse_command(char *line, char *args[])
+{
+	char *token;
+	int i = 0;
+
+	token = strtok(line, " \t\n");
+	while (token && i < 99)
+	{
+	args[i++] = token;
+	token = strtok(NULL, " \t\n");
+	}
+	args[i] = NULL;
+	return (i);
+}
