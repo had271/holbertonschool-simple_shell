f15a52ed059f8ab46ee3e05bd40cb83b4e60d8d2
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-11 10:24:47 +0300

    Update help_func2.c

--- help_func2.c
+++ help_func2.c
@@ -1,94 +1,109 @@
 #include "main.h"
 
 /**
- * myshell_cd - function to change directory
- * @args: pointer
- * Return: 1 or exit
-*/
-
-int myshell_cd(char **args)
+	* get_path_env - retrieves the value of the PATH environment variable
+	* @envp: array of environment variables
+	*
+	* Return: pointer to the PATH string (skipping "PATH=") if found,
+	*         NULL otherwise
+	*/
+char *get_path_env(char **envp)
 {
-	if (args == NULL || args[1] == NULL)
+	int i = 0;
+
+	if (!envp)
+	return (NULL);
+
+	while (envp[i])
 	{
-		write(STDOUT_FILENO, "Please enter a command\n", 23);
-		return (1);
+	if (strncmp(envp[i], "PATH=", 5) == 0)
+	return (envp[i] + 5);
+	i++;
 	}
-	if (chdir(args[1]) != 0)
+	return (NULL);
+}
+
+/**
+	* handle_builtins - executes built-in commands if found
+	* @args: tokenized command
+	* @line: input line to free if exit
+	* @envp: environment variables
+	* @status: pointer to shell status
+	*
+	* Return: 1 if a built-in was executed, 0 otherwise
+	*/
+int handle_builtins(char *args[], char *line, char **envp, int *status)
+{
+	int i;
+
+	if (!args[0])
+	return (1);
+
+	if (strcmp(args[0], "exit") == 0)
 	{
-		perror("cd");
-		return (1);
+	free(line);
+	exit(*status);
+	}
+
+	if (strcmp(args[0], "env") == 0)
+	{
+	for (i = 0; envp[i]; i++)
+	{
+	write(STDOUT_FILENO, envp[i], strlen(envp[i]));
+	write(STDOUT_FILENO, "\n", 1);
 	}
 	return (1);
-}
-
-/**
- * myshell_env - function that prints the current environment
- * @args: pointer
- * Return: 1
-*/
-
-int myshell_env(__attribute__((unused)) char **args)
-{
-	char **envir;
-
-	for (envir = environ; *envir; envir++)
-	{
-		write(STDOUT_FILENO, *envir, _strlen(*envir));
-		write(STDOUT_FILENO, "\n", 1);
 	}
-return (1);
+	return (0);
 }
 
 /**
- * myshell_help - function to perfom help from shell
- * @args: pointer
- * Return: 0
-*/
-
-int myshell_help(__attribute__((unused)) char **args)
+	* handle_env - prints the current environment if command is "env"
+	* @args: array of command arguments
+	* @envp: array of environment variables
+	*
+	* Return: 1 if the command was "env", 0 otherwise
+	*/
+int handle_env(char *args[], char **envp)
 {
-	write(STDOUT_FILENO, "Help list \n", 11);
-	write(STDOUT_FILENO, "cd - change the current directory\n", 33);
-	write(STDOUT_FILENO, "exit - cause normal process termination\n", 39);
-	write(STDOUT_FILENO, "help - display useful information\n", 33);
+	int j;
+
+	if (strcmp(args[0], "env") == 0)
+	{
+	for (j = 0; envp[j]; j++)
+	{
+	write(STDOUT_FILENO, envp[j], strlen(envp[j]));
+	write(STDOUT_FILENO, "\n", 1);
+	}
 	return (1);
+	}
+	return (0);
 }
 
 /**
- * myshell_exit - function to exit from shell
- * @args: pointer
- * Return: 0, always success
-*/
-
-int myshell_exit(__attribute__((unused)) char **args)
+	* handle_exit - exits the shell if command is "exit"
+	* @args: array of command arguments
+	* @line: input line to free
+	* @status: current exit status
+	*
+	* Return: void (exits the program if "exit" is called)
+	*/
+void handle_exit(char *args[], char *line, int status)
 {
-	return (2);
-}
+	if (strcmp(args[0], "exit") == 0)
+	{
+	free(line);
+	exit(status);
+	}
 
 /**
- * read_command - read the command that the user will type
- * in my shell and stores it in a buffer
- * Return: the buffer
-*/
-
-char *read_command(void)
+ * is_executable - checks if a file is executable
+ * @path: path to the file
+ *
+ * Return: 1 if the file is executable, 0 otherwise
+ */
+int is_executable(char *path)
 {
-	size_t line_size = 0;
-	char *line = NULL;
-
-#if defined(_GNU_SOURCE)
-	if (getline(&line, &line_size, stdin) == -1)
-	{
-		free(line);
-		exit(0);
-	}
-#else
-	if (_getline(&line, (ssize_t *)&line_size, stdin) == -1)
-	{
-		free(line);
-		exit(0);
-	}
-#endif
-	return (line);
+	return (access(path, X_OK) == 0);
 }
-
+}
