e41f3c686e28536183f5ea43abd76a23f1ea7795
HADEEL ALMUTAIRI <147671513+had271@users.noreply.github.com>
2025-11-11 11:01:36 +0300

    Update help_func2.c

--- help_func2.c
+++ help_func2.c
@@ -92,13 +92,13 @@ int handle_env(char *args[], char **envp)
 int handle_exit(char **args, int last_status) 
 {
     int status;
-    
+    char *endptr;
+
     if (args[1] == NULL) {
         exit(last_status);
     }
-    
-    char *endptr;
-    long num = strtol(args[1], &endptr, 10);
+
+	long num = strtol(args[1], &endptr, 10);
     
     if (*endptr != '\0' || args[1][0] == '\0') {
         fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", args[1]);
@@ -120,4 +120,5 @@ int is_executable(char *path)
 {
 	return (access(path, X_OK) == 0);
 }
+
 
